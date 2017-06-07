1b46465a04ab25ccd48e84e13a4c5f3bb66d0727
Your Name <youremail@domain.com>
2017-06-07 15:24:11 +0300

    initial commit with a ll routes created

--- app/router.js
+++ app/router.js
@@ -7,6 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('company');
+  this.route('advertise');
+  this.route('apps');
+  this.route('labs');
+  this.route('terms');
+  this.route('privacy');
+  this.route('dcma');
+  this.route('contacts');
 });
 
 export default Router;
